a295ec1ce35d41e8e6f279ee9d8b9206bb6fb4b6
Arnold Ndiwalana <arnoldndiwalana@gmail.com>
2019-03-28 19:51:47 +0300

    show newly created contact

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,42 @@
-// Import contact function from contacts.js
-//const contact = require('./contact.js');
-//import {contact} from './contact.js';
-//const shuffle = require('./shuffler.js');
+
 $(document).ready(function(){
 
+
+  // hides the contact-List
+  $("#info").hide();
+  // Address Book Array to store the contacts created
+  var addressBook =[]
+  var count = 0;
+
+  /*Searches through the addressBook for a contact with a matching ID
+  -The mini array containing the contact details in the address book is in the format
+    [this.ID,this.firstName,[this]]
+  -So the searchByID() function will reference the addressBook[j][0] which is the index of the ID idNumber
+    and if a matching ID is found, it will print the addressBook[j][2] which is the index of the contact details.
+  */
+  /*var searchByFirstName = (f_name) => {
+    contactExists = false;
+    for(j=0; j<addressBook.length ; j++){
+      if(addressBook[j][1] === f_name){
+
+        //console.log("Contact found - ",addressBook[j][1]);
+        //console.log(addressBook[j][2]);
+        //contactExists = true;
+      };
+    }
+    //if(contactExists === false){
+        //console.log("There is no contact with that ID.")
+    //};
+  };*/
   $("#submit").click(function(event) {
-    // Address Book Array to store the contacts created
-    var addressBook =[]
+    // Prevent the page from reloading
+    event.preventDefault();
+
+    // shows the contact List
+    $("#info").show("slow");
+
+
+
     // Variables to get information from the form fields
     var firstName = document.getElementById('fname').value
     var lastName = document.getElementById('lname').value
@@ -17,51 +47,50 @@ $(document).ready(function(){
     var country = document.getElementById('country').value
 
     //This is the new contact constructor function.
-     function contact (firstName, secondName, phoneNumber, email, street, city, country){
-      this.firstName = firstName;
+     function contact (firstNames, secondName, phoneNumber, emails, streets, citys, countrys){
+      this.firstName = firstNames;
       this.secondName = secondName;
       this.phoneNumber = phoneNumber;
-      this.email = email;
+      this.email = emails;
       this.address = {}; //Makes a new object from this.address
-      this.address.street = street; //pushes street to this.address
-      this.address.city = city; //pushes city to this.address
-      this.address.country = country; //pushes country to this.address
-      this.ID = Math.floor((Math.random() * 100000000) + 200000000);
-      //A random ID number between 100000000 and 200000000 is assigned to new contacts.
-      this.fullAddress = function(){
+      this.address.street = streets; //pushes street to this.address
+      this.address.city = citys; //pushes city to this.address
+      this.address.country = countrys; //pushes country to this.address
+      this.ID = count; // counts the number of addresses added
+
+       /*this.fullAddress = function(){
         console.log (this.address.street + ", " + this.address.city + ", " + this.address.country);
-      }; //When the this.fullAddress() function is called, it prints  the street, city and country of the contact.
+      };*/ //When the this.fullAddress() function is called, it prints  the street, city and country of the contact.
       addressBook.push([this.ID,this.firstName,[this]]);
       //pushes the new contact contained within its own mini array to the address book in the format [this.ID,this.firstName,[this]]
-      
+
+
     }
 
     //Function to add contacts to the homepage
-    $("#address-list").append(addressBook[2]);
 
-    /*Searches through the addressBook for a contact with a matching ID
-    -The mini array containing the contact details in the address book is in the format
-      [this.ID,this.firstName,[this]]
-    -So the searchByID() function will reference the addressBook[j][0] which is the index of the ID idNumber
-      and if a matching ID is found, it will print the addressBook[j][2] which is the index of the contact details.
-    */
-    var searchByFirstName = (f_name) => {
-      contactExists = false;
-      for(j=0; j<addressBook.length ; j++){
-        if(addressBook[j][1] === f_name){
-          //console.log("Contact found - ",addressBook[j][1]);
-          //console.log(addressBook[j][2]);
-          //contactExists = true;
-        };
-      }
-      //if(contactExists === false){
-          //console.log("There is no contact with that ID.")
-      //};
-    };
+
 
     // Function call for contact
     var person = new contact (firstName, lastName, phone, email, street, city, country);
-    alert(addressBook);
 
+    //$("#info").append(addressBook[count][2][0].secondName);
+  alert(addressBook);
+  alert(addressBook[count][2][0]);
+  $('#contactForm').trigger('reset');
+
+      $("#info").append(`<p>
+        <a data-toggle="collapse" href="#` + addressBook[count][2][0].secondName + `" role="link" aria-expanded="false" aria-controls="`+ addressBook[count][2][0].secondName +`">` + addressBook[count][2][0].firstName +  `</a><br>
+        <div class="collapse multi-collapse" id="`+ addressBook[count][2][0].secondName   +`"> `+ addressBook[count][2][0].secondName + `\n` + addressBook[count][2][0].phoneNumber + `</div>
+      </p>
+  `)
+  count++;
   });
+  $("#show-contact").click(function(){
+
+    //alert(addressBook);
+    //alert(count);
+  });
+
+
 });

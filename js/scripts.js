a1be97eabcf585f4eb93552f545757d4e8c74207
“Paul <prowori2@gmail.com>
2019-03-30 12:32:13 +0300

    Add comments explaining the code

--- js/scripts.js
+++ js/scripts.js
@@ -5,38 +5,16 @@ $(document).ready(function(){
 
   // hides the contact-List
   $("#info").hide();
+
   // Address Book Array to store the contacts created
   var addressBook =[];
-  var clickFunctions = [];
   var count = 0;
 
 
-  /*Searches through the addressBook for a contact with a matching ID
-  -The mini array containing the contact details in the address book is in the format
-    [this.ID,this.firstName,[this]]
-  -So the searchByID() function will reference the addressBook[j][0] which is the index of the ID idNumber
-    and if a matching ID is found, it will print the addressBook[j][2] which is the index of the contact details.
-  */
-  /*var searchByFirstName = (f_name) => {
-    contactExists = false;
-    for(j=0; j<addressBook.length ; j++){
-      if(addressBook[j][1] === f_name){
-
-        //console.log("Contact found - ",addressBook[j][1]);
-        //console.log(addressBook[j][2]);
-        //contactExists = true;
-      };
-    }
-    //if(contactExists === false){
-        //console.log("There is no contact with that ID.")
-    //};
-  };*/
   $("#submit").click(function(event) {
     // Prevent the page from reloading
     event.preventDefault();
 
-    // shows the contact List
-    //$("#info").show("slow");
 
 
 
@@ -61,47 +39,41 @@ $(document).ready(function(){
       this.address.country = countrys; //pushes country to this.address
       this.ID = count; // counts the number of addresses added
 
-       /*this.fullAddress = function(){
-        console.log (this.address.street + ", " + this.address.city + ", " + this.address.country);
-      };*/ //When the this.fullAddress() function is called, it prints  the street, city and country of the contact.
       addressBook.push([this.ID,this.firstName,[this]]);
       //pushes the new contact contained within its own mini array to the address book in the format [this.ID,this.firstName,[this]]
 
 
-    }
+    };
 
-    //Function to add contacts to the homepage
+  // Function call for contact, creating a new object in the addressBook array using details from the form
+  var person = new contact (firstName, lastName, phone, email, street, city, country);
 
 
-
-    // Function call for contact
-    var person = new contact (firstName, lastName, phone, email, street, city, country);
-
-    //$("#info").append(addressBook[count][2][0].secondName);
-  alert(addressBook);
-  alert(addressBook[count][2][0]);
+  //Resets the form every time a new contact is entered.
   $('#contactForm').trigger('reset');
 
 
-
+  /*
+  Appends a div containing the first name of the new contact to the info div (which is invisible by default) and adds
+  the contact details as a separate div right under the firstName div
+  */
  $("#info").append(`
   <div>
     <a href="#" class="collapsible" id = "`+ person.firstName + person.ID +`">`+ person.firstName +`</a>
     <div class="content" id = "`+ person.secondName + person.ID +`">
-    First Name:`+ person.firstName + `<br> Second Name:`+ person.secondName +`<br>Phone number:`+ person.phoneNumber
-    +`<br>Email Address:`+ person.email +`<br>Street:`+ person.address.street +`<br>City:`+ person.address.city +`<br>Country:`+ person.address.country +`
+    First Name: `+ person.firstName + `<br> Second Name: `+ person.secondName +`<br>Phone number: `+ person.phoneNumber
+    +`<br>Email address: `+ person.email +`<br>Street: `+ person.address.street +`<br>City: `+ person.address.city +`<br>Country: `+ person.address.country +`
     </div>`)
 
+    //Hides the div containing contact details so that only the contact firstName is visible.
     $(`#`+ person.secondName + person.ID +``).hide();
 
-
-
+    //Assigns variables to make accessing the contact name and contact details easier
     var coll = document.getElementsByClassName("collapsible");
-    var content = document.getElementById(``+ addressBook[count][2][0].secondName + addressBook[count][2][0].ID +``);
-    //content.style.display === "none"
+    var content = document.getElementById(``+ person.secondName + person.ID +``);
 
-    $(`#`+ addressBook[count][2][0].firstName + addressBook[count][2][0].ID +``).click(function() {
-      //this.classList.toggle("active");
+    //Assigns a collapsible effect to the contact details div such that when the contact firt name is clicked, it toggles the visibility of the details div
+    $(`#`+ person.firstName + person.ID +``).click(function() {
       if (content.style.display === "block") {
         content.style.display = "none";
       } else {
@@ -109,27 +81,25 @@ $(document).ready(function(){
       }
     });
 
+  //Increments count to keep track of the number of contacts.
   count++;
   alert("Contact added successfuly. " + count + " contacts currently in address book.")
   });
 
+  //Adds functionality to the show-contact button such that it shows the contact list and hides the form
   $("#show-contact").click(function(event) {
     $("#info").show("slow");
     $("#contactForm").hide("slow");
 
   });
 
+  //Adds functionality to the back-form button such that it shows the form and hides the contact list
   $("#back-form").click(function(event) {
     $("#info").hide("slow");
     $("#contactForm").show("slow");
 
   });
 
-  //$("#show-contact").click(function(){
-
-    //alert(addressBook);
-    //alert(count);
-  //});
 
 
 });
